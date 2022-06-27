a38c0f747314143fbb9e459e4292ae2dde9a6f4b
Ryan Taylor <ryan.b.taylor@avanade.com>
2022-06-27 11:23:30 -0400

    added comments to explain gasprices function code

--- function/gasprices.cs
+++ function/gasprices.cs
@@ -38,25 +38,30 @@ namespace gavanade.function
             int index = 0;
             string searchstring = "", responseMessage = "";
 
+            // if the latitude is not null or empty try to parse
             if (!string.IsNullOrEmpty(latitude_str))
             {
                 log.LogInformation("latitude passed");
                 Double.TryParse(latitude_str, out latitude);
             }
+            // if the longitude is not null or empty try to parse
             if (!string.IsNullOrEmpty(longitude_str))
             {
                 log.LogInformation("longitude passed");
                 Double.TryParse(longitude_str, out longitude);
             }
 
+            // if given coordinates, convert them to zipcode
             if (latitude != 0 & longitude != 0)
             {
+                // use api to get zipcode from coordiantes
                 client.DefaultRequestHeaders.Add("x-ms-client-id", "46f031d2-20fb-4ca3-b4f3-217ceeaa9d1a");
                 var response = await client.GetAsync(
                     $"https://atlas.microsoft.com/search/address/reverse/json?api-version=1.0&query={latitude},{longitude}&subscription-key=QtkUpdIsoJvs1Di_m2zLOIe_lPCTQEdGUHnZZCTIQuU"
                 );
                 responseMessage = await response.Content.ReadAsStringAsync();
 
+                // search for zipcode in the response from api
                 searchstring = "\"postalCode\":\"";
                 index = responseMessage.IndexOf(searchstring) + searchstring.Length;
                 zipcode_str = responseMessage.Substring(index);
@@ -64,43 +69,54 @@ namespace gavanade.function
             }
             else
             {
+                // if no coordinates are given, query api with given zipcode instead
                 client.DefaultRequestHeaders.Add("x-ms-client-id", "46f031d2-20fb-4ca3-b4f3-217ceeaa9d1a");
                 var response = await client.GetAsync(
                     $"https://atlas.microsoft.com/search/address/json?api-version=1.0&query={zipcode_str}&countrySet=US&subscription-key=QtkUpdIsoJvs1Di_m2zLOIe_lPCTQEdGUHnZZCTIQuU"
                 );
                 responseMessage = await response.Content.ReadAsStringAsync();
             }
+
+            // if the zipcode is not null or empty try to parse
             if (!string.IsNullOrEmpty(zipcode_str))
             {
                 log.LogInformation("zipcode passed");
                 Int32.TryParse(zipcode_str, out zipcode);
             }
+            // if zipcode is still 0 return an error
             if (zipcode == 0)
             {
                 return new OkObjectResult("Error with the query,0,0,0,0,0");
             }
+
+            // search for state abbreviation in the response from api
             searchstring = "\"countrySubdivision\":\"";
             index = responseMessage.IndexOf(searchstring) + searchstring.Length;
             state_abbr = responseMessage.Substring(index);
             state_abbr = state_abbr.Substring(0, state_abbr.IndexOf("\""));
 
+            // search for state in the response from api
             searchstring = "\"countrySubdivisionName\":\"";
             index = responseMessage.IndexOf(searchstring) + searchstring.Length;
             state = responseMessage.Substring(index);
             state = state.Substring(0, state.IndexOf("\""));
 
+            // search for city in response from api
             searchstring = "\"municipality\":\"";
             index = responseMessage.IndexOf(searchstring) + searchstring.Length;
             city = responseMessage.Substring(index);
             city = city.Substring(0, city.IndexOf("\""));
 
+            // if given a zipcode, get prices from gasbuddy
             if (zipcode != 0)
             {
+                // webscrape the prices from gasbuddy
                 var response = await client.GetAsync(
                     $"https://www.gasbuddy.com/home?search={zipcode}"
                 );
                 responseMessage = await response.Content.ReadAsStringAsync();
 
+                // search for the average price in string returned from gasbuddy
                 searchstring = "<span class=\"text__lg___1S7OO text__bold___1C6Z_ text__left___1iOw3 PriceTrends-module__priceHeader___fB9X9\">";
 
                 index = responseMessage.IndexOf(searchstring) + searchstring.Length;
@@ -111,18 +127,22 @@ namespace gavanade.function
                 areaprice = price;
             }
 
+            // use state abbreviation to webscrape AAA for national and state averages
             if (!string.IsNullOrEmpty(state_abbr))
             {
+                // webscrape the average prices from AAA
                 var response = await client.GetAsync(
                     $"https://gasprices.aaa.com/?state={state_abbr}"
                 );
                 responseMessage = await response.Content.ReadAsStringAsync();
 
+                // search for the national average in string returned from AAA
                 searchstring = "AAA National Average ";
                 index = responseMessage.IndexOf(searchstring) + searchstring.Length;
                 nationalprice = responseMessage.Substring(index);
                 nationalprice = nationalprice.Substring(0, nationalprice.IndexOf(" "));
 
+                // search for the state average in string returned from AAA
                 searchstring = $"AAA {state} Avg. ";
                 index = responseMessage.IndexOf(searchstring) + searchstring.Length;
                 stateprice = responseMessage.Substring(index);
@@ -130,6 +150,7 @@ namespace gavanade.function
             }
 
 
+            // update result with zipcode, city, state and all prices
             result = $"{zipcode},{city},{state},{areaprice},{stateprice},{nationalprice}";
             return new OkObjectResult(result);
         }
