8e061532b068181a5ae303240fb14f85c64aab35
Ryan Taylor <ryan.b.taylor@avanade.com>
2022-07-11 17:14:34 -0400

    added comments to explain database function code

--- function/database.cs
+++ function/database.cs
@@ -25,14 +25,16 @@ namespace gavanade.function
         {
             log.LogInformation("Processing request for database.");
 
+            // request which table to run via query
             string table = req.Query["table"];
+
             string result = "";
             string responseMessage = "";
             string searchString = "";
             string price = "";
-
             string query = "";
 
+            // credentials for accessing sql database
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
             builder.DataSource = "gavanade-db-server.database.windows.net";
             builder.UserID = "gavanade";
@@ -40,23 +42,27 @@ namespace gavanade.function
             builder.InitialCatalog = "gavanade-db";
 
             int index = 0;
+            // create list with all national and state prices
             List<string> prices = new List<string>();
 
             int tableNum = 0;
+            // pastPrices table = 1
             if (table == "pastPrices")
             {
                 tableNum = 1;
             }
+            // contactInformation table = 2
             else if (table == "contactInformation")
             {
                 tableNum = 2;
             }
+            // concerns table = 2
             else if (table == "concerns")
             {
                 tableNum = 3;
             }
 
-
+            // if unknown table was given, ERROR CHECKING
             if (tableNum == 0)
             {
                 // TODO error checking
@@ -65,33 +71,42 @@ namespace gavanade.function
             {
                 switch (tableNum)
                 {
+                    // if pastPrices table was given 
                     case 1:
                         log.LogInformation("Processing past prices.");
 
+                        // webscrape state averages page from AAA
                         var response = await client.GetAsync(
                             "https://gasprices.aaa.com/state-gas-price-averages/"
                         );
                         responseMessage = await response.Content.ReadAsStringAsync();
 
+                        // search for the national average in string returned from AAA
                         searchString = "Today’s AAA National Average $";
                         index = responseMessage.IndexOf(searchString) + searchString.Length;
+
+                        // if searchstring not found, return an error
                         if (index <= 0)
                         {
                             log.LogInformation("Failed to find search string");
                         }
+                        // if searchstring found, add national price to price string
                         else
                         {
                             price = responseMessage.Substring(index);
                             price = price.Substring(0, price.IndexOf(" ") - 1);
+                            // add national price to prices list
                             prices.Add(price);
                         }
 
-                        // search for the average price in string returned from AAA
+                        // loop through AAA response and find each state average
                         for (int i = 0; i < 51; i++)
                         {
+                            // search for the state average in string returned from AAA
                             searchString = "<td class=\"regular\" style=\"display: table-cell;\">$";
                             index = responseMessage.IndexOf(searchString) + searchString.Length;
 
+                            // if searchstring not found, return an error
                             if (index <= 0)
                             {
                                 log.LogInformation("Failed to find search string");
@@ -99,6 +114,7 @@ namespace gavanade.function
                             }
                             price = responseMessage.Substring(index);
                             price = price.Substring(0, price.IndexOf(" ") - 1);
+                            // add each price to the prices list
                             prices.Add(price);
                             responseMessage = responseMessage.Substring(index);
                         }
@@ -108,12 +124,14 @@ namespace gavanade.function
                         {
                             connection.Open();
 
+                            // delete the previous prices from database
                             query = "DELETE FROM [dbo].[pastPrices]";
                             using (SqlCommand command = new SqlCommand(query, connection))
                             {
                                 command.ExecuteNonQuery();
                             }
 
+                            // insert new prices into the database and the date it was added
                             query = $"INSERT INTO [dbo].[pastPrices] VALUES ('{DateTime.Now}'";
                             foreach (string p in prices)
                             {
@@ -129,14 +147,16 @@ namespace gavanade.function
                             connection.Close();
                         }
 
-
                         break;
+                    // if contactInformation table was given
                     case 2:
                         log.LogInformation("Processing contact information.");
                         break;
+                    // if concerns table was given
                     case 3:
                         log.LogInformation("Processing concerns.");
                         break;
+                    // if unknown table was given return an error
                     default:
                         log.LogInformation("error");
                         break;
